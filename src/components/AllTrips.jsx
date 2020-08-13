3b7397caba7de1b2b3ad225ecb34fdef459b425a
Senthil <spk2dc@virginia.edu>
2020-08-13 16:25:58 -0400

    working on sorting all dat a returned from database

--- src/components/AllTrips.jsx
+++ src/components/AllTrips.jsx
@@ -1,24 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import Map from "./Map";
 
 export default function AllTrips() {
+  // use localhost if environment url does not exist
+  const baseURL = process.env.REACT_APP_API_URL || "http://localhost:8000";
+
+  // State Hook
+  const [allData, setAllData] = useState([]);
+  const [places, setPlaces] = useState([]);
+  const [activities, setActivities] = useState([]);
+
   // get uploaded file from database and extract data
-  const getDatabaseFile = () => {
+  const getDatabaseFile = (event) => {
+    event.preventDefault();
     axios
       .get(`${baseURL}/api/v1/all_maps/`)
       .then((resp) => {
-        console.log(`getDatabaseFile -> resp`, resp.data.data[0].data);
-        document.getElementById("database_file").textContent =
-          resp.data.data[0].data;
+        console.log(`getDatabaseFile -> resp`, resp.data.data);
+        // document.getElementById("database_file").textContent = resp.data.data[0].data;
+        setAllData([...resp.data.data]);
+        sortTrips();
       })
       .catch((err) => {
         console.log(`getDatabaseFile -> err`, err);
       });
   };
 
+  const sortTrips = () => {
+    allData.map((val, i) => {
+      val.timelineObjects.map((item, j) => {
+        return;
+      });
+    });
+  };
+
   return (
     <div>
       <h1>All Trips</h1>
+      <button
+        onClick={(e) => {
+          getDatabaseFile(e);
+        }}
+      >
+        Refresh Data
+      </button>
+      <table>
+        <caption>File Name: </caption>
+        <thead>
+          <tr>
+            <th>Place</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {allData.map((val, i) => {
+            return (
+              <tr>
+                <td>{val.data[0]}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
       <Map />
     </div>
   );
