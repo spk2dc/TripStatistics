69f0ded5dbf570f2d6ca7d488c8d3ea2c030a041
Senthil <spk2dc@virginia.edu>
2020-08-13 14:14:29 -0400

    adding navbar

--- src/App.js
+++ src/App.js
@@ -3,10 +3,23 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Map from "./components/Map";
 import UploadFile from "./components/UploadFile";
+import NavBar from "./components/NavBar";
 
 function App() {
+  // State Hook
+  const [sessionUser, setSessionUser] = useState("");
+
+  // Get session function
+  const getSession = () => {
+    const userString = sessionStorage.getItem("user");
+    const user = userString ? JSON.parse(userString) : userString;
+    setSessionUser(user);
+  };
+
+  // Render
   return (
     <div className='App'>
+      <NavBar getSession={getSession} sessionUser={sessionUser} />
       <UploadFile />
       <Map />
     </div>
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,55 @@
+// Imports
+import React, { useEffect } from "react";
+import { Nav, Navbar, Button } from "react-bootstrap";
+
+export default function NavBar({ getSession, sessionUser }) {
+  // Logout on click handler
+  const destroySession = () => {
+    sessionStorage.setItem("user", "");
+    getSession();
+  };
+
+  useEffect(() => {
+    getSession();
+  }, []);
+
+  // Render
+  return (
+    <Navbar
+      collapseOnSelect
+      expand='lg'
+      bg='dark'
+      variant='dark'
+      className='fixed-top'
+    >
+      <img id='favicon' src='/favicon.ico' alt='' />
+      <Navbar.Brand id='nav-title' href='/'>
+        Trip Statistics
+      </Navbar.Brand>
+
+      <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+      <Navbar.Collapse id='responsive-navbar-nav'>
+        <Nav className='mr-auto'>
+          <Nav.Link href='/'>Home</Nav.Link>
+          <Nav.Link href='/signup'>Sign Up</Nav.Link>
+          {sessionUser ? (
+            <>
+              <Nav.Link href='/profile'>Profile</Nav.Link>
+              <Nav.Link href='/all_trips'>All Trips</Nav.Link>
+              <Button
+                type='button'
+                id='btnLogout'
+                onClick={destroySession}
+                href='/'
+              >
+                Logout
+              </Button>
+            </>
+          ) : (
+            <Nav.Link href='/login'>Login</Nav.Link>
+          )}
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
