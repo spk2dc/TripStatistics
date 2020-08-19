39a9e37137e11cddef5886ba4fb4e2b42a93d543
Senthil <spk2dc@virginia.edu>
2020-08-19 02:32:41 -0400

    changing landing page text and putting it in a container

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -9,13 +9,21 @@ export default function LandingPage({ getSession, sessionUser }) {
 
   return (
     <div id='div-landing' className='jumbotron text-center'>
-      <h1>Trip Statistics</h1>
-      <div>
-        <p>Sign Up or Login to analyze your trips!</p>
-        <a className='btn btn-outline-success m-2' href='/register'>
+      <div className='container shadow p-3 mb-5 rounded container-landing'>
+        <h1>Trip Statistics</h1>
+        <p>
+          Sign Up or Login to analyze your trips from Google Maps Location
+          History!
+        </p>
+        <a href='https://takeout.google.com/settings/takeout' target='_blank'>
+          Click here to downloaded your location data from Google
+        </a>
+        <br />
+        <br />
+        <a className='btn btn-outline-success m-3' href='/register'>
           Register
         </a>
-        <a className='btn btn-outline-success m-2' href='/login'>
+        <a className='btn btn-outline-success m-3' href='/login'>
           Login
         </a>
       </div>
